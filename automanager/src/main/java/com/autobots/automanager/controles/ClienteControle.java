51e0a26227df9eb361de707ebd1bae48614a8de9
Julia Gayotto <juliagayotto@gmail.com>
2023-06-07 23:17:31 -0300

    Adicao dos codigos da lista 1

--- automanager/src/main/java/com/autobots/automanager/controles/ClienteControle.java
+++ automanager/src/main/java/com/autobots/automanager/controles/ClienteControle.java
@@ -11,15 +11,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.autobots.automanager.adicionadores.AdicionadorLinkCliente;
 import com.autobots.automanager.entidades.Cliente;
-import com.autobots.automanager.modelos.AdicionadorLinkCliente;
-import com.autobots.automanager.modelos.ClienteAtualizador;
-import com.autobots.automanager.modelos.ClienteSelecionador;
+import com.autobots.automanager.modelo.ClienteAtualizador;
+import com.autobots.automanager.modelo.ClienteSelecionador;
 import com.autobots.automanager.repositorios.ClienteRepositorio;
 
 @RestController
+@RequestMapping("/cliente")
 public class ClienteControle {
 	@Autowired
 	private ClienteRepositorio repositorio;
@@ -55,7 +57,7 @@ public class ClienteControle {
 		}
 	}
 
-	@PostMapping("/cliente/cadastro")
+	@PostMapping("/cadastro")
 	public ResponseEntity<?> cadastrarCliente(@RequestBody Cliente cliente) {
 		HttpStatus status = HttpStatus.CONFLICT;
 		if (cliente.getId() == null) {
@@ -66,7 +68,7 @@ public class ClienteControle {
 
 	}
 
-	@PutMapping("/cliente/atualizar")
+	@PutMapping("/atualizar")
 	public ResponseEntity<?> atualizarCliente(@RequestBody Cliente atualizacao) {
 		HttpStatus status = HttpStatus.CONFLICT;
 		Cliente cliente = repositorio.getById(atualizacao.getId());
@@ -81,7 +83,7 @@ public class ClienteControle {
 		return new ResponseEntity<>(status);
 	}
 
-	@DeleteMapping("/cliente/excluir")
+	@DeleteMapping("/excluir")
 	public ResponseEntity<?> excluirCliente(@RequestBody Cliente exclusao) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		Cliente cliente = repositorio.getById(exclusao.getId());
